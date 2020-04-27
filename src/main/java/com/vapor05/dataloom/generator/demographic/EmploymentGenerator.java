e981ce24f6487a7d296aaec126562e6ff699950e
NBOCCHINI <nicholasbocchini@gmail.com>
2020-04-26 23:59:39 -0500

    Added the EmploymentGenerator class

---
+++ src/main/java/com/vapor05/dataloom/generator/demographic/EmploymentGenerator.java
@@ -0,0 +1,115 @@
+package com.vapor05.dataloom.generator.demographic;
+
+import com.vapor05.dataloom.databus.DataArray;
+import com.vapor05.dataloom.databus.DataMap;
+import com.vapor05.dataloom.databus.DataMapException;
+import com.vapor05.dataloom.generator.AbstractGenerator;
+import com.vapor05.dataloom.json.JSONTokener;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+
+/**
+ *
+ * @author NicholasBocchini
+ */
+public class EmploymentGenerator extends AbstractGenerator {
+
+    private DataArray<DataMap> source;
+    private double unemployed = 0.05;
+    private String salaryKey = "salary";
+    private String birthDateKey = "birthDate";
+    
+    public EmploymentGenerator()
+    {
+        source = new DataArray(new JSONTokener(getClass().getResourceAsStream("/config/employment.json")));
+    }
+    
+    public EmploymentGenerator(String key, String salaryKey, String birthDateKey)
+    {
+        this();   
+        this.key = key;
+        this.salaryKey = salaryKey;
+        this.birthDateKey = birthDateKey;
+    }
+
+    public void setUnemployed(double unemployed)
+    {
+        this.unemployed = unemployed;
+    }
+
+    public void setSalaryKey(String salaryKey)
+    {
+        this.salaryKey = salaryKey;
+    }
+
+    public void setBirthDateKey(String birthDateKey)
+    {
+        this.birthDateKey = birthDateKey;
+    }
+    
+    @Override
+    public DataMap generate(DataMap record) throws DataMapException
+    {
+        LocalDate dob;
+        DataMap employment;
+        int age;
+        int dollar;
+       
+        if (!record.containsKey(birthDateKey))
+        {
+            age = 18;
+        }
+        else
+        {
+            dob = LocalDate.ofInstant(Instant.ofEpochMilli(record.getLong(birthDateKey)), ZoneId.of("Z"));
+            age = Period.between(dob, LocalDate.now()).getYears();
+        }
+    
+        if (age >= 18)
+        {
+            if (random.nextDouble() <= unemployed)
+            {
+                record.put(key, "Unemployed");
+            }
+            else 
+            {
+                employment = getEmploymentInformation(random.nextDouble());
+                record.put(key, employment.getString(key));
+                dollar = random.nextInt(3000) * (random.nextBoolean() ? -1 : 1);
+                record.put(salaryKey, employment.getInt(salaryKey) + dollar);
+            }
+        }
+        
+        return record;
+    }
+    
+    private DataMap getEmploymentInformation(double split) throws DataMapException
+    {
+        DataMap employment = new DataMap();
+        DataArray<String> jobNames;
+        
+        for (DataMap jobInfo : source)
+        {
+            if (jobInfo.getDouble("start") <= split && split < jobInfo.getDouble("end"))
+            {
+                if (jobInfo.getDataArray("employment_names").isEmpty())
+                {
+                    employment.put(key, jobInfo.getString("employment_group"));
+                }
+                else
+                {
+                    jobNames = jobInfo.getDataArray("employment_names");
+                    employment.put(key, jobNames.get(random.nextInt(jobNames.size())));
+                }
+                
+                employment.put(salaryKey, jobInfo.getInt("annual_median_salary"));
+                break;
+            }
+        }
+        
+        return employment;
+    }
+    
+}
---
+++ src/test/java/com/vapor05/dataloom/generator/demographic/EmploymentGeneratorTest.java
@@ -0,0 +1,78 @@
+package com.vapor05.dataloom.generator.demographic;
+
+import com.vapor05.dataloom.databus.DataMap;
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ *
+ * @author NicholasBocchini
+ */
+public class EmploymentGeneratorTest {
+
+
+    @Test
+    public void testSomeMethod() throws Exception
+    {
+        EmploymentGenerator generator = new EmploymentGenerator();
+        DataMap data = new DataMap();
+        DataMap timeData = new DataMap();
+        long start;
+        long elapsed;
+        long unemployed = 0;
+        
+        generator.setSeed(1L);
+        generator.setKey("job");
+        data = generator.generate(data);
+        assertTrue(data.containsKey("job"));
+        assertTrue(data.containsKey("salary"));
+        assertTrue(data.get("job") instanceof String);
+        assertFalse(data.getString("job").isEmpty());        
+        
+        generator.setBirthDateKey("dob");
+        data = new DataMap("{dob:1420092000000}"); //5years old
+        data = generator.generate(data);
+        assertFalse(data.containsKey("job"));
+        data = new DataMap("{dob:1009864800000}"); //18years old
+        data = generator.generate(data);
+        assertTrue(data.containsKey("job"));
+        data = new DataMap("{dob:1262325600000}"); //10years old
+        data = generator.generate(data);
+        assertFalse(data.containsKey("job"));
+        data = new DataMap("{dob:473407200000}"); //35years old
+        data = generator.generate(data);
+        assertTrue(data.containsKey("job"));
+        data = new DataMap("{dob:1514786400000}"); //2years old
+        data = generator.generate(data);
+        assertFalse(data.containsKey("job"));
+        
+        for (int i=0; i<100000; i++) 
+        {
+            data = generator.generate(new DataMap());
+            assertTrue(data.containsKey("job"));
+            
+            if (data.getString("job").equals("Unemployed")) unemployed++;
+        }
+
+        assertEquals(0.05, (unemployed/(float)100000), 0.005);
+        assertTimeout(ofMillis(1), () -> {
+            generator.generate(timeData);
+        });
+        
+        start = System.nanoTime();
+        generator.generate(timeData);
+        elapsed = System.nanoTime() - start;
+        System.out.println("EmploymentGenerator execution time (ms): " + elapsed/(float)1000000);
+        
+        generator.setSeed(122L);
+        data = generator.generate(data);
+        System.out.println(data);
+        
+    }
+    
+    
+}
